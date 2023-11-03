0f6b1252c7ae33db0cfd620189dc3b2f5e635bd3
rafael.ponte@zup.com.br <rafael.ponte@zup.com.br>
2023-11-03 14:59:02 +0000

    stk-cli: create app initial commit.

---
+++ demo-app-1103b/src/main/java/br/com/zup/cinema/demo/DemoApp1103bApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.cinema.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApp1103bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApp1103bApplication.class, args);
+	}
+
+}
